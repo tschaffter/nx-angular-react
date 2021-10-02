ed50996b5fe17aebfd98e0df5101088eb5c832b8
tschaffter <thomas.schaffter@gmail.com>
2021-10-02 09:57:43 -0700

    Fix unknown greeting component in angular test

--- apps/angularapp/src/app/app.component.spec.ts
+++ apps/angularapp/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
